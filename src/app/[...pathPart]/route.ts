c92a7683ff559e55426c40b2b3bd1059e1902450
Patrick Creehan <pcreehan@users.noreply.github.com>
2024-10-21 09:15:05 -0400

    In-memory redirector

--- src/app/[...pathPart]/route.ts
+++ src/app/[...pathPart]/route.ts
@@ -1,6 +1,30 @@
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
-export async function GET(): Promise<NextResponse> {
-  return NextResponse.redirect(new URL("https://www.microsoft.com"));
-}
+import { NextRequest } from "next/server";
+
+// In-memory Map for URL redirects
+const redirectMap = new Map([
+  ["example", "https://example.com"],
+  ["google", "https://google.com"],
+  ["servant", "https://servant.io"],
+  // Add more mappings as needed
+]);
+type RedirectFunc = (request: NextRequest, { params }: { params: { pathPart: string[] } }) => NextResponse;
+
+export const GET: RedirectFunc = (request, { params }) => handleRedirect(params.pathPart, 307); // Temporary redirect for GE
+export const POST: RedirectFunc = (request, { params }) => handleRedirect(params.pathPart, 308);
+export const PUT = POST;
+export const PATCH = POST;
+export const DELETE = POST;
+
+const handleRedirect = (pathPart: string[], statusCode: number): NextResponse => {
+  const path = pathPart.join("/");
+  const redirectUrl = redirectMap.get(path);
+
+  if (redirectUrl) {
+    return NextResponse.redirect(redirectUrl, { status: statusCode });
+  } else {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+};
